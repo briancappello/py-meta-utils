cddf31bb77d8ce4a5ea4c07088de98c047153a5e
Brian Cappello <briancappello@gmail.com>
2018-09-24 14:56:24 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='Py Meta Utils',
+    version='0.0.1',
+    description='Metaclass utilities for Python',
+    long_description=long_description,
+    url='https://github.com/briancappello/py-meta-utils',
+    author='Brian Cappello',
+    license='MIT',
+
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    py_modules=['py_meta_utils'],
+    install_requires=[],
+    extras_require={
+        'docs': [
+            'm2r',
+            'sphinx',
+            'sphinx-autobuild',
+            'sphinx-rtd-theme',
+        ],
+    },
+    python_requires='>=3.5',
+    include_package_data=True,
+    zip_safe=False,
+)
