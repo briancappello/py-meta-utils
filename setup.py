c163c6585f3430f4befbc716cca5c3745f19ead0
Brian Cappello <briancappello@gmail.com>
2018-10-23 11:11:16 -0600

    release v0.7.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Py Meta Utils',
-    version='0.7.0',
+    version='0.7.1',
     description='Metaclass utilities for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
